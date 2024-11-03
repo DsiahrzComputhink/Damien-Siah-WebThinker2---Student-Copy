db0f6ec1b64e1ff2cfe71d0b6df1dd0eb2851ff3
DsiahrzComputhink <damiensiah11@gmail.com>
2024-11-03 04:28:50 +0000

    message

--- Damien-Siah-WebThinker2-Student-Copy/script/hangman.js
+++ Damien-Siah-WebThinker2-Student-Copy/script/hangman.js
@@ -1,23 +1,27 @@
 let words = ['BRAZIL', 'PIZZA', 'SOCCER', 'EIFFEL TOWER', 'PIKACHU', 'DHOBY GHAUT', 'CARPE DIEM', 'CANADA', 'BAGUETTE', 'BASEBALL', 'PYRAMIDS', 'CHARIZARD', 'BISHAN', 'LIVE AND LEARN', 'CHINA', 'LASAGNA', 'BASKETBALL', 'STONEHENGE', 'BULBASAUR', 'PASIR RIS', 'LESS IS MORE', 'DENMARK', 'SUSHI', 'VOLLEYBALL', 'TAJ MAHAL', 'JIGGLYPUFF', 'JURONG EAST', 'LIFE GOES ON', 'EGYPT', 'BURRITO', 'BADMINTON', 'COLOSSEUM', 'MEOWTH', 'RAFFLES PLACE', 'BELIEVE IN YOU', 'FRANCE', 'RISOTTO', 'CRICKET', 'MACHU PICCHU', 'PSYDUCK', 'OUTRAM PARK', 'DARE TO DREAM', 'GERMANY', 'PANCAKE', 'RUGBY', 'GREAT WALL', 'ONIX', 'YISHUN', 'TIME FLIES FAST', 'HUNGARY', 'OMELETTE', 'GOLF', 'PETRA', 'SQUIRTLE', 'SEMBAWANG', 'TO THINE OWN SELF BE TRUE', 'ICELAND', 'FALAFEL', 'TENNIS', 'ACROPOLIS', 'VAPOREON', 'TAMPINES', 'LOVE CONQUERS ALL', 'JAPAN', 'GOULASH', 'HOCKEY', 'ANGKOR WAT', 'SNORLAX', 'SERANGOON', 'KNOWLEDGE IS POWER', 'KENYA', 'KIMCHI', 'ARCHERY', 'CHRIST THE REDEEMER', 'MEWTWO', 'WOODLANDS', 'PATIENCE IS KEY', 'LAOS', 'TEMPURA', 'JUDO', 'SYDNEY OPERA HOUSE', 'TOGEPI', 'CLEMENTI', 'LAUGHTER IS MEDICINE', 'MEXICO', 'QUINOA', 'FENCING', 'MOUNT RUSHMORE', 'CYNDAQUIL', 'CHINATOWN', 'JOY IN THE JOURNEY', 'NAMIBIA', 'GUACAMOLE', 'KARATE', 'LOUVRE MUSEUM', 'LUGIA', 'BUGIS', 'CHANGE IS CONSTANT', 'OMAN', 'CHURROS', 'SAILING', 'BUCKINGHAM PALACE', 'MUDKIP', 'LAVENDER', 'PEACE BEGINS WITHIN', 'PERU', 'TOFU', 'WRESTLING', 'NOTRE-DAME', 'RAYQUAZA', 'BUKIT BATOK', 'ART IS FREEDOM', 'QATAR', 'WAFFLE', 'LACROSSE', 'SISTINE CHAPEL', 'LUCARIO', 'PUNGGOL', 'DREAM BIG FEAR LITTLE', 'RUSSIA', 'PAELLA', 'POLO', 'GOLDEN GATE BRIDGE', 'CHIMCHAR', 'HARBOURFRONT', 'STRENGTH IN UNITY', 'SPAIN', 'CEVICHE', 'BOXING', 'NIAGARA FALLS', 'GIRATINA', 'CITY HALL', 'EVERY END IS A NEW BEGINNING', 'TURKEY', 'BAKLAVA', 'ROWING', 'TOWER OF PISA', 'OSHAWOTT', 'KRANJI', 'HOPE ANCHORS THE SOUL', 'UGANDA', 'COUSCOUS', 'SURFING', 'CHICHEN ITZA', 'ZEKROM', 'ORCHARD', 'VIETNAM', 'TORTILLA', 'SKATEBOARDING', 'HAGIA SOPHIA', 'FROAKIE', 'EUNOS', 'YEMEN', 'BIRYANI', 'TAEKWONDO', 'PARTHENON', 'GOOMY', 'NOVENA', 'ZIMBABWE', 'RATATOUILLE', 'GYMNASTICS', 'BRANDENBURG GATE', 'ROWLET', 'SENGKANG', 'IRELAND', 'BRUSCHETTA', 'BOBSLEIGH', 'TABLE MOUNTAIN', 'MIMIKYU', 'PAYA LEBAR', 'GREECE', 'POUTINE', 'TRIATHLON', 'EDINBURGH CASTLE', 'TOXTRICITY', 'EXPO', 'BHUTAN', 'HUMMUS', 'HANDBALL', 'GRAND CANYON', 'WOOLOO', 'KENT RIDGE', 'BOLIVIA', 'TABOULEH', 'SQUASH', 'MOUNT FUJI', 'DRACOVISH', 'BOTANIC GARDENS', 'FIJI', 'CANNOLI', 'TABLE TENNIS', 'NGORONGORO CRATER', 'RILLABOOM', 'KHATIB', 'NEPAL', 'MUFFIN', 'CURLING', 'SAGRADA FAMILIA', 'CUFANT', 'BUANGKOK', 'LIGMA', 'AMONG US', 'SIGMA', 'ALPHA', 'BETA']
-const word = floor[Math.random() *words.length];
+let word;
+let incorrectGuesses = [];
 let guess = [];
 let life = 7;
 
+word = words[Math.floor(Math.random() * words.length)];
 const keyPress = (key) => {
-    if(guess.kindexOf(key) == -1){
+    console.log(word);
+    if(guess.indexOf(key) == -1){
         // addd letter to guess list
         guess.push(key);
-        if(word.indexOf(key) !=-1){
+        if(word.indexOf(key) != -1){
             $('#hint').html('');
             let found = true;
             // update the hint
             for(let i = 0;i<word.length;i++){
                 if(word[i] == " "){
                     $('#hint').append('<div>' + space + "</div>");
-                }else if(guess.indexOf(world[i])!=-1){
-                    $(('#hint').append('<div>' + word[i] + '</div>'));
+                }else if(guess.indexOf(word[i])!=-1){
+                    $('#hint').append('<div>' + word[i] + '</div>');
                 }else{
                     $('#hint').append('<div>' + "_" + '</div>')
+                    found = false;
                 }
             }
 
@@ -26,6 +30,10 @@ const keyPress = (key) => {
             }
         }
         else {
+            life -= 1;
+            console.log(life)
+            $('life').attr("src",'img/hangman'+life+'.svg');
+            incorrectGuesses.push(key);
             if(life == 0){
                 console.log('player has lost')
             }
@@ -33,15 +41,15 @@ const keyPress = (key) => {
     }
 }
 
-const SubmitGuess = () =>
+function SubmitGuess ()
 {
-    console.log(word);
-    let guess = $('#guessInput').val().toUpperCase();
+    
+    let guess = $('#GuessInput').val().toUpperCase();
 
     if (guess.length === 1 && /^[A-Z]$/.test(guess)) {
         keyPress(guess);
 
-        $('#guessInput').val('')
+        $('#GuessInput').val('')
     } else {
 
         alert('Please enter a valid letter')
@@ -55,5 +63,5 @@ const newGame = () =>
 
 const endGame = () =>
 {
-    $
+    
 }
